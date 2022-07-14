df7679c9eb29899b4bb24222cbc26c4574a8b496
Vaibhav Sood <vaibhavsood12@gmail.com>
2022-07-14 23:35:23 +0530

    Add thread safety for singleton pattern

--- design_patterns/singleton/Logger.cpp
+++ design_patterns/singleton/Logger.cpp
@@ -3,13 +3,16 @@
 #include<iomanip>
 
 Logger* Logger::instance_ = nullptr;
+std::mutex Logger::lock_;
 int Logger::counter = 0;
 
 Logger* Logger::getLogger() {
+        Logger::lock_.lock();
         if(!Logger::instance_) {
             counter++;
             std::cout << "counter:" << counter << std::endl;
             Logger::instance_ = new Logger();
         }
+        Logger::lock_.unlock();
         return Logger::instance_;
 }
--- design_patterns/singleton/Logger.h
+++ design_patterns/singleton/Logger.h
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<iomanip>
 #include<string>
+#include<mutex>
 
 class Logger {
     static int counter;
     static Logger* instance_;
+    static std::mutex lock_;
     Logger() { }//counter++; }
 public:
     static Logger* getLogger();
